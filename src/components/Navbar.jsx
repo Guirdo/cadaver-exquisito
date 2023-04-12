43c0c1b625606cc6ac1dddeb176e31f8229b6222
Guirdo <produccionesaldo@gmail.com>
2023-04-12 15:32:17 -0600

    🧩 Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { A } from "@solidjs/router";
+
+export default function Navbar() {
+  return(
+    <nav className="p-block-md">
+      <h1 className="text-align-center">
+        <A
+          class="text-dec-none color-inherit"
+          href="/"
+        >
+          💀Cadaver exquisito🍷
+        </A>
+      </h1>
+    </nav>
+  )
+}
