1596a70e3e56c9564b529c2102ef2de19c8e8d19
Guirdo <produccionesaldo@gmail.com>
2023-04-15 14:58:53 -0600

    🗃️ Return to a local store * 🗃️ Add and uuid to user

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,8 +1,9 @@
-import { createLocalStore } from "../components/utils/createLocalStore";
+import { createLocalStore } from "../utils/createLocalStore";
+import { v4 as uuidv4 } from 'uuid'
 
 const initialState = {
+  id: uuidv4(),
   nickname: 'MuertadoLopez'
 }
 
 export const [user,setUser] = createLocalStore('user',initialState)
-
