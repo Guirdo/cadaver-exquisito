2327ec9857a65e353bbf61f48321e86555bc9800
Guirdo <produccionesaldo@gmail.com>
2023-04-17 20:18:03 -0600

    🧩 Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+  return(
+    <footer class="flex-column align-items-center p-xs">
+      <p class="fs-sm">
+        Hecho con ❤️ por <a href="https://guirdo.xyz">Guirdo</a>
+      </p>
+    </footer>
+  )
+}
