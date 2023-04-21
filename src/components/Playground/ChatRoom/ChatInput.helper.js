c72776cd6f0ae6cdba88de60d5a1a05bebe40650
Guirdo <produccionesaldo@gmail.com>
2023-04-21 14:42:56 -0600

    🦺 Validate if user can send a new message * ♻️ Refactor use of ChatInput helpers functions

--- src/components/Playground/ChatRoom/ChatInput.helper.js
+++ src/components/Playground/ChatRoom/ChatInput.helper.js
@@ -2,18 +2,16 @@ import DOMPurify from 'dompurify'
 import isEmpty from "validator/es/lib/isEmpty"
 import { setErrorMessage } from '../../../stores/error'
 
-export default function isMessageValid(message) {
-  const sanitizedText = sanitizeText(message)
-
-  if(isEmpty(sanitizedText)) {
-    setErrorMessage('Has dejado el campo vació o has ingresado un mensaje invalido')
+export function isMessageValid(message) {
+  if(isEmpty(message)) {
+    setErrorMessage('invalidMessage')
     return false
   }
   
   return true
 }
 
-function sanitizeText(text) {
+export function sanitizeText(text) {
   return DOMPurify.sanitize(text,{
     ALLOWED_TAGS: []
   })
--- src/components/Playground/ChatRoom/ChatInput.jsx
+++ src/components/Playground/ChatRoom/ChatInput.jsx
@@ -1,45 +1,79 @@
 import { createEffect, createSignal } from "solid-js"
 import { room, sendMessage } from "../../../stores/room"
 import { user, setUser } from "../../../stores/user"
-import isMessageValid from "./ChatInput.helper"
+import { isMessageValid, sanitizeText } from "./ChatInput.helper"
 import { useI18n } from "@solid-primitives/i18n"
 
 export default function ChatInput() {
-  const [ t ] = useI18n()
+  const [t] = useI18n()
   const [message, setMessage] = createSignal('')
+  const [charCount, setChatCount] = createSignal(0)
+  const [enable, setEnable] = createSignal(false)
   const playerTurn = room.players.findIndex(p => p.id === user.id)
-  
+  const charLimit = 70;
+
   createEffect(() => {
-    setUser('allowedToWrite',room.messages.length % room.playersLimit === playerTurn)
+    setUser('allowedToWrite', room.messages.length % room.playersLimit === playerTurn)
   })
 
-  const handleSend = (e) => {
-    e.preventDefault()
+  const handleInput = (e) => {
+    const input = e.target.value
+    setMessage(input)
+    setChatCount(input.length)
 
-    if(isMessageValid(message())){
-      sendMessage(message())
-      setMessage('')
+    if (input.length > charLimit || input.length === 0) {
+      setEnable(false)
+    } else {
+      setEnable(true)
     }
   }
 
-  return(
-    <form class="flex-row gap-xs">
+  const isDisabled = () => {
+    if (user.allowedToWrite && enable()) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const handleSend = (e) => {
+    e.preventDefault()
+    const sanitizedText = sanitizeText(message())
+
+    if (isMessageValid(sanitizedText)) {
+      sendMessage(sanitizedText)
+    }
+
+    setMessage('')
+    setChatCount(0)
+  }
+
+  return (
+    <form class="[ flex-row ] [ gap-xs ]">
       <textarea
+        class="flex-grow"
         rows={3}
         value={message()}
         disabled={!user.allowedToWrite}
-        onInput={(e) => setMessage(e.target.value)}
+        onInput={handleInput}
       />
 
-      <button
-        class="button"
-        data-type="success"
-        type="submit"
-        onClick={handleSend}
-        disabled={!user.allowedToWrite}
-      >
-        {t('chatRoom.send')}
-      </button>
+      <div class="[ flex-column ] [ justify-content-between gap-xs ]">
+        <button
+          class="button"
+          data-type="success"
+          type="submit"
+          onClick={handleSend}
+          disabled={isDisabled()}
+        >
+          {t('chatRoom.send')}
+        </button>
+        <span
+          class={isDisabled() ? 'color-primary' : 'color-success'}
+        >
+          {charCount()}/{charLimit}
+        </span>
+      </div>
     </form>
   )
 }
