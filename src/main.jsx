814e01b2712935319cf5365c3f5aec925bc4da3d
Guirdo <produccionesaldo@gmail.com>
2023-04-21 11:31:56 -0600

    🍱 Add Aleo font * 🔧 Add unfonts plugin to the vite configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { render } from 'solid-js/web'
+import 'unfonts.css'
 import './styles/global.css'
 import App from './App'
 import { Router } from '@solidjs/router'
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import Unfonts from 'unplugin-fonts/vite'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    Unfonts({
+      custom:{
+        families:[{
+          name: 'Aleo',
+          local: 'Aleo',
+          src: './public/fonts/*.woff2',
+          transform(font) {
+            if (font.basename === 'Aleo-Bold') {
+              font.weight = 700
+            }
+            return font
+          }
+        }],
+        preload: true,
+      }
+    })
+  ],
 });
