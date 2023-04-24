8b6436e8e7e928263e3b5804d1fe894170b354c5
Guirdo <produccionesaldo@gmail.com>
2023-04-24 14:09:51 -0600

    🦺 Validate if user enter an url in the chat

--- src/components/Playground/ChatRoom/ChatInput.helper.js
+++ src/components/Playground/ChatRoom/ChatInput.helper.js
@@ -2,17 +2,27 @@ import DOMPurify from 'dompurify'
 import isEmpty from "validator/es/lib/isEmpty"
 import { setErrorMessage } from '../../../stores/error'
 
+const urlRegex = /(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/g
+
+function contains(text) {
+  const matches = urlRegex.exec(text)
+  return matches && matches.length > 0
+}
+
 export function isMessageValid(message) {
-  if(isEmpty(message)) {
+  if (isEmpty(message)) {
+    setErrorMessage('invalidMessage')
+    return false
+  } else if (contains(message, urlRegex)) {
     setErrorMessage('invalidMessage')
     return false
   }
-  
+
   return true
 }
 
 export function sanitizeText(text) {
-  return DOMPurify.sanitize(text,{
+  return DOMPurify.sanitize(text, {
     ALLOWED_TAGS: []
   })
 }
