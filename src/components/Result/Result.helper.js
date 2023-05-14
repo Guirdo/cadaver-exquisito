f7f70b37460818f708cc286053ca0e42cc909996
Guirdo <produccionesaldo@gmail.com>
2023-05-14 13:16:57 -0600

    🗃️ Add Result.helper for fetching a result

---
+++ src/components/Result/Result.helper.js
@@ -0,0 +1,16 @@
+import { supabase } from "../../supabase"
+
+export async function fetchResult(id) {
+  try {
+    const { data, error } = await supabase
+      .from('rooms')
+      .select('players, messages')
+      .eq('id', id)
+
+    if (error) throw new Error(error.message)
+
+    return data[0]
+  } catch (error) {
+    console.log(error)
+  }
+}
