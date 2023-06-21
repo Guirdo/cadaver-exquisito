c89cdfebafceb6d1de9a1676a7c14b95a9f5e9df
Guirdo <produccionesaldo@gmail.com>
2023-06-21 13:41:03 -0600

    🧩 Move MessageBox component to its own file

---
+++ src/components/Playground/ChatRoom/MessageBox.jsx
@@ -0,0 +1,36 @@
+import { room } from "../../../stores/room"
+import { user } from "../../../stores/user"
+
+export default function MessageBox(props) {
+  const playerTurn = room.players.findIndex(p => p.id === user.id)
+  const isUserMessage = () => {
+    return props.index % room.players.length === playerTurn
+  }
+
+  return (
+    <div
+      class={
+        `[ ${isUserMessage() ? 'flex-row-reverse' : 'flex-row'} ]
+          [ gap-xs align-items-center ]`
+      }
+    >
+      <img
+        src="/icons/skull.webp"
+        width="40"
+        height="40"
+      />
+
+      <div
+        class={`
+          ${isUserMessage() ? 'bg-info color-white' : 'bg-secondary'}
+          flex-grow p-xs
+        `}
+      >
+        {props.index + 1 === room.messages.length && user.allowedToWrite ?
+          props.message :
+          '...'
+        }
+      </div>
+    </div>
+  )
+}
