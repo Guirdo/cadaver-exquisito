e1754f084459ea4e6945444bc8f303cdd72d0cc8
Guirdo <produccionesaldo@gmail.com>
2023-10-06 14:44:56 -0600

    🧩 Add button for opening the settings modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,38 @@
 import { A } from "@solidjs/router";
+import settingsIcon from '/icons/settings.svg'
+import { setUI } from "../stores/ui";
 
 export default function Navbar() {
   return (
-    <nav className="[ flex-row ] [ bg-primary color-white gap-xxs align-items-center justify-content-center p-block-sm ]">
-      <figure class="w-2rem">
-        <img
-          src="/icons/skull.webp"
-        />
-      </figure>
-      <h1 className="text-align-center">
-        <A
-          class="text-dec-none color-inherit"
-          href="/"
-        >
-          Cadáver exquisito
-        </A>
-      </h1>
-      <figure class="w-2rem">
-        <img
-          src="/icons/wine.webp"
-        />
-      </figure>
+    <nav className="[ flex-row ] [ bg-primary color-white align-items-center p-sm ]">
+      <div className="[ flex-row ] [ flex-grow align-items-center gap-xxs ]">
+        <figure class="w-2rem">
+          <img
+            src="/icons/skull.webp"
+          />
+        </figure>
+        <h1 className="text-align-center xs-fs-lg">
+          <A
+            class="text-dec-none color-inherit"
+            href="/"
+          >
+            Cadáver exquisito
+          </A>
+        </h1>
+        <figure class="w-2rem">
+          <img
+            src="/icons/wine.webp"
+          />
+        </figure>
+      </div>
+      <button
+        className="button border-none" 
+        data-type="primary"
+        onClick={() => setUI('openSettings', v => true)
+      }
+      >
+        <img src={settingsIcon} height={24} />
+      </button>
     </nav>
   )
 }
