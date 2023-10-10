38c052927166c3050918a5b3ceec755a242a8236
Guirdo <produccionesaldo@gmail.com>
2023-10-10 15:39:27 -0600

    💄 Update Modal styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,8 +3,8 @@ import { Portal } from 'solid-js/web'
 export default function ErrorModal(props) {
   return (
     <Portal>
-      <div class="modal">
-        <div class="modal-content">
+      <div class="[ flex-row modal ] [ w-100 h-100 justify-content-center align-items-center ]">
+        <div class="[ modal-content ] [ p-lg bg-theme color-theme m-inline-lg ]">
           {props.children}
         </div>
       </div>
