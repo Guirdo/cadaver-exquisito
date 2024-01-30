6d9552d22ead13600a70318d9c5b69b5055364af
Guirdo <produccionesaldo@gmail.com>
2024-01-30 15:05:10 -0600

    🧩 Add RandomPublicRoomBoard component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,83 +1,22 @@
-import { useNavigate } from "@solidjs/router"
-import { user, setUser } from "../../stores/user"
-import isNicknameValid from "../../helpers/isNicknameValid"
+import { setUser } from "../../stores/user"
 import { onMount } from "solid-js"
-import { clearRoom, createRoom } from "../../stores/room"
-import { findPublicRoom } from "../../stores/public_room"
+import { clearRoom } from "../../stores/room"
 import InfoSection from "./InfoSection"
-import { t } from '../../i18n'
+import JoinRoom from "./JoinRoom"
+import RandomPublicRoomBoard from "./RandomPublicRoomBoard"
 
 export default function HomePage() {
-  const navigate = useNavigate()
-
-  onMount(() =>{
+  onMount(() => {
     setUser('isOwner', false)
     clearRoom()
   })
 
-  const ASSIGNING_USER = {
-    createRoom: async () => {
-      const roomId = await createRoom(user)
-      setUser('isOwner', true)
-      navigate(`/${roomId}`)
-    },
-    joinPublicRoom: async() => {
-      const publicRoomId = await findPublicRoom()
-      navigate(`/p/${publicRoomId}`)
-    }
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    if (isNicknameValid()) {
-      let submitter = e.submitter.value
-
-      ASSIGNING_USER[submitter]()
-    }
-  }
-
   return (
-    <div class="[ flex-column ] [ w-100 gap-lg p-md align-items-center ]">
-      <figure class="w-6rem">
-        <img src="/icons/skull.webp" />
-      </figure>
-
-      <form
-        class="[ flex-column ] [ gap-sm ]"
-        onSubmit={handleSubmit}
-      >
-        <label
-          class="fw-bold "
-          for="nickname"
-        >
-          {t('common.enterNickname')}
-        </label>
-        <input
-          id="nickname"
-          name="nickname"
-          value={user.nickname}
-          onInput={(e) => setUser('nickname', e.target.value)}
-        />
-
-        <button
-          class="button"
-          data-type="success"
-          type="submit"
-          value="createRoom"
-        >
-          {t('homePage.createRoom')}
-        </button>
-
-        <button
-          class="button"
-          data-type="info"
-          type="submit"
-          value="joinPublicRoom"
-        >
-          {t('homePage.joinPublicRoom')}
-        </button>
-      </form>
+    <div id="home-page" class="[ flex-column ] [ w-100 gap-lg p-md align-items-center ]">
+      <div class="[ flex-row ] [ align-items-center gap-lg flex-wrap w-100 justify-content-evenly ]">
+        <JoinRoom />
+        <RandomPublicRoomBoard />
+      </div>
 
       <InfoSection />
     </div>
---
+++ src/components/HomePage/RandomPublicRoomBoard.jsx
@@ -0,0 +1,36 @@
+import { A } from "@solidjs/router";
+import { For, createResource, createSignal, onMount } from "solid-js";
+import { getMostRecentPublicRooms, getRandomPublicRoom } from "../../stores/public_room";
+import cutOffText from "../../utils/cutOffText";
+import { t } from "../../i18n";
+
+export default function RandomPublicRoomBoard() {
+  const [randomCadaver, setRandomCadaver] = createSignal('')
+  const [mostRecentPublicRooms] = createResource(getMostRecentPublicRooms)
+
+  onMount(() => {
+    getRandomPublicRoom()
+      .then(setRandomCadaver)
+  })
+
+  return (
+    <div class="flex-column">
+      <h2>{t('publicRoom.seeWhatOthersWrote')}</h2>
+      <A class="p-block-s-xs" href={`/p/${randomCadaver()}`}>
+      {t('publicRoom.randomCadaver')}
+      </A>
+      <h3 class="p-block-s-sm">{t('publicRoom.threeMostRecent')}</h3>
+      <ul>
+        <For each={mostRecentPublicRooms()} fallback={<li>{t('common.loading')}</li>}>
+          {(item) => (
+            <li>
+              <A href={`/p/${item.id}`}>
+                {cutOffText(item.title)}
+              </A>
+            </li>
+          )}
+        </For>
+      </ul>
+    </div >
+  )
+}
