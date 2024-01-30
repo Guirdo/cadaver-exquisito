3732945e23c6fb0f2a2069dd852f2f095ded163c
Guirdo <produccionesaldo@gmail.com>
2024-01-30 15:04:35 -0600

    🧩 Add JoinRoom component

---
+++ src/components/HomePage/JoinRoom.jsx
@@ -0,0 +1,78 @@
+import { useNavigate } from "@solidjs/router"
+import { createRoom } from "../../stores/room"
+import { findPublicRoom } from "../../stores/public_room"
+import { t } from '../../i18n'
+import { user } from "../../stores/user"
+import isNicknameValid from "../../helpers/isNicknameValid"
+
+export default function JoinRoom() {
+  const navigate = useNavigate()
+  const ASSIGNING_USER = {
+    createRoom: async () => {
+      const roomId = await createRoom(user)
+      setUser('isOwner', true)
+      navigate(`/${roomId}`)
+    },
+    joinPublicRoom: async () => {
+      const publicRoomId = await findPublicRoom()
+      navigate(`/p/${publicRoomId}`)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (isNicknameValid()) {
+      let submitter = e.submitter.value
+
+      ASSIGNING_USER[submitter]()
+    }
+  }
+
+  return (
+    <form
+      class="[ flex-column ] [ justify-content-center align-items-center gap-sm ]"
+      onSubmit={handleSubmit}
+    >
+      <div class="flex-column">
+        <label
+          class="fw-bold fs-sm p-block-e-xxs"
+          for="nickname"
+        >
+          {t('common.enterNickname')}
+        </label>
+        <input
+          id="nickname"
+          class="w-100 fs-sm"
+          name="nickname"
+          value={user.nickname}
+          onInput={(e) => setUser('nickname', e.target.value)}
+        />
+      </div>
+
+      <figure class="w-4rem">
+        <img src="/icons/skull.webp" />
+      </figure>
+
+      <div class="flex-column w-100">
+        <button
+          class="button"
+          data-type="success"
+          type="submit"
+          value="joinPublicRoom"
+        >
+          {t('homePage.joinPublicRoom')}
+        </button>
+
+        <button
+          class="button fs-sm"
+          data-type="info"
+          type="submit"
+          value="createRoom"
+        >
+          {t('homePage.createRoom')}
+        </button>
+      </div>
+    </form>
+  )
+}
