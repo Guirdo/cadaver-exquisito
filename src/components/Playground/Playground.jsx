b91e2a34e51b4562d852abb7b2c386443f15a35a
Guirdo <produccionesaldo@gmail.com>
2024-01-29 19:07:51 -0600

    🩹 Move subscription inside fetchRoom method

--- src/components/Playground/Playground.jsx
+++ src/components/Playground/Playground.jsx
@@ -1,6 +1,6 @@
 import { Match, Switch } from "solid-js";
 import { useParams } from "@solidjs/router";
-import { room, fetchRoom, subscribeToRoomChanges } from "../../stores/room";
+import { room, fetchRoom } from "../../stores/room";
 import { lazy } from 'solid-js'
 
 const WaitingRoom = lazy(() => import('./WaitingRoom/WaitingRoom'))
@@ -32,10 +32,6 @@ function StatusSwitcher() {
 export default function Playground() {
   const params = useParams()
   fetchRoom(params.id)
-
-  if(room.status !== 2){
-    subscribeToRoomChanges()
-  }
 
   return (
     <div class="[ playground ] [ flex-column ] [ flex-grow gap-lg p-sm align-items-center ]">
