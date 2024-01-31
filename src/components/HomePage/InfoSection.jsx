33a5b465af0bd2e208562e93739ded95275b079a
Guirdo <produccionesaldo@gmail.com>
2024-01-31 12:48:50 -0600

    🧩 Add SectionTemplate subcomponent in InfoSection

--- src/components/HomePage/InfoSection.jsx
+++ src/components/HomePage/InfoSection.jsx
@@ -1,15 +1,28 @@
 import { t } from '../../i18n'
 
+function SectionTemplate(props) {
+  return (
+    <section class="border-1">
+      <div class="w-100 p-xs bg-primary color-white">
+        <h2>{t(props.title)}</h2>
+      </div>
+      <div>
+        <div class="[ flex-column ] [ gap-xs fs-sm p-sm ]">
+          {props.children}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+
 export default function InfoSection() {
   return (
-    <div class="[ flex-row ] [ flex-wrap fs-sm ]">
-      <details class="w-28ch">
-        <summary>{t('homePage.about')}</summary>
-        <div class="[ flex-column ] [ gap-xs ]">
-          <p>{t('homePage.description1')}</p>
-          <p>{t('homePage.description2')}</p>
-        </div>
-      </details>
+    <div class="[ grid-info-section ] [ gap-md align-items-start w-100 ]">
+      <SectionTemplate title='homePage.about'>
+        <p>{t('homePage.description1')}</p>
+        <p>{t('homePage.description2')}</p>
+      </SectionTemplate>
     </div>
   )
 }
