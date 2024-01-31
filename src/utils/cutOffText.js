977b37378ab348428612f7880006fd24a982f442
Guirdo <produccionesaldo@gmail.com>
2024-01-31 12:44:32 -0600

    👔 Increment textLength in cutOffText utility

--- src/utils/cutOffText.js
+++ src/utils/cutOffText.js
@@ -1,5 +1,5 @@
 export default function cutOffText(text){
-  const textLength = 21
+  const textLength = 28
   if(text.length > textLength){
       text = text.substring(0, textLength) + '...';
   }
